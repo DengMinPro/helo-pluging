d954b7d9ad3f38360543d3ad24e58a460d5121ca
GitHub Action <action@github.com>
2024-07-06 08:17:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/dengminpro/helopluging/MyBundle.kt
+++ src/main/kotlin/com/github/dengminpro/helopluging/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dengminpro.helopluging
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dengminpro/helopluging/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/dengminpro/helopluging/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dengminpro.helopluging.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
